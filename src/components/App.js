5297cfcce4db1de5787cc1072e731e26b6ef25df
brenokamura <brenokamura@gmail.com>
2022-07-24 03:51:18 -0300

    frist commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+
+import Tela01 from './Tela01.js';
+
+
+export default function App() {
+    return (
+      <div>
+        <Tela01/>
+      </div>
+    );
+}
---
+++ src/components/Tela01.js
@@ -0,0 +1,11 @@
+export default function Tela01(){
+    return(
+        <div className="tela01">
+            <img  src="assets/img/logo.png" alt="logo" />
+            <h1>ZapRecall</h1>
+            <button >Iniciar Recall!</button>
+        </div>
+
+    );
+}
+
